6ad12b0c2e23101151a90c1c36881d9f000f251e
Senth <senth.wallace@gmail.com>
2019-03-28 14:02:26 +0100

    Update random_user_delay() to use binominal distribution

--- facebookcli/utils.py
+++ facebookcli/utils.py
@@ -4,5 +4,5 @@ from time import sleep
 
 def random_user_delay():
     """Delays the script by a random amount to make us pass for a human"""
-    delay = random() * 10.0 + 3.0
+    delay = (random() + random()) * 6.0 + 3.0
     sleep(delay)
